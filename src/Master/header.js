f5a13d6e1f708751c30a1dcbb0f2407d407ee869
wwiiinn11 <75421679+wwiiinn11@users.noreply.github.com>
2021-09-23 12:15:34 +0700

    Update header

--- src/Master/header.js
+++ src/Master/header.js
@@ -69,6 +69,7 @@ export default function Footer() {
                 | LAPTOP |
               </Link>
             </Button>
+            
             <Button>
               <Link
                 to="/kasir"
